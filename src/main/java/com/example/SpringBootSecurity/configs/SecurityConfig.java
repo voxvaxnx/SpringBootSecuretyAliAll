2987a3798eb02162bf397885532f14ea9ead45ab
voxvaxnx <voxvaxnx@yandex.ru>
2023-03-22 19:00:25 +0500

    работает аутонтификация

---
+++ src/main/java/com/example/SpringBootSecurity/configs/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.SpringBootSecurity.configs;
+
+import com.example.SpringBootSecurity.services.PersonDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PersonDetailsService personDetailsService;
+
+    @Autowired
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
---
+++ src/main/java/com/example/SpringBootSecurity/services/PersonDetailsService.java
@@ -0,0 +1,33 @@
+package com.example.SpringBootSecurity.services;
+
+import com.example.SpringBootSecurity.models.Person;
+import com.example.SpringBootSecurity.repositories.PeopleRepository;
+import com.example.SpringBootSecurity.securety.PersonDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PersonDetailsService implements UserDetailsService {
+
+    private final PeopleRepository peopleRepository;
+
+    @Autowired
+    public PersonDetailsService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        Optional<Person> person = peopleRepository.findByUsername(s);
+
+        if (person.isEmpty())
+            throw new UsernameNotFoundException("User not found");
+
+        return new PersonDetails(person.get());
+    }
+}
